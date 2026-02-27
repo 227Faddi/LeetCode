101b15dd9f2d9dcb3d3b8bd76d605f192071edc6
227Faddi <falukhouma27@gmail.com>
2026-02-27 10:50:16 -0500

    chore: update solution

--- neetcode/validPalindrome.js
+++ neetcode/validPalindrome.js
@@ -1,25 +1,32 @@
 class Solution {
-  /**
-   * @param {string}
-   * @return {boolean}
-   */
-  // receive a string, case sensitive
-  // ignore non alpha numeric charts
-  // return a boolean
-  // always a string, from 1 to 1k char
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    // receive a string, case sensitive
+    // ignore non alpha numeric charts
+    // return a boolean
+    // always a string, from 1 to 1k char
 
-  //PREP
-  // clean the input
-  // create a reversed variable
-  // compare the 2
-  // return the result
+    isPalindrome(s) {
+        const cleanS = (s) => {
+            return s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "")
+        }
 
-  isPalindrome(s) {
-    const validS = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-    const reversedS = validS.split("").reverse().join("");
+        const clean = cleanS(s)
 
-    const result = validS === reversedS;
+        let l = 0
+        let r = clean.length - 1
 
-    return result;
-  }
+        while(l <= r){
+            if(clean[l] === clean[r]){
+                l++
+                r--
+            } else{
+                return false
+            }
+        }
+
+        return true
+    }
 }
