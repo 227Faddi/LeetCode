5b4fee0e3d70b6ce9a948535d0d7232480259f52
227Faddi <falukhouma27@gmail.com>
2026-03-05 10:42:09 -0500

    chore: update solution

--- neetcode/twosum.js
+++ neetcode/twosum.js
@@ -1,29 +1,22 @@
 class Solution {
-  /**
-   * @param {number[]} nums
-   * @param {number} target
-   * @return {number[]}
-   */
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    
+    twoSum(nums, target) {
+        const map = new Map()
 
-  // we receive an array of integers and a target number
-  // return an array containing the 2 indices of the numbers that add up to the target
-  //  Input: nums = [3,4,5,6], target = 7
-  // Output: [0,1]
+        for(let i = 0; i < nums.length; i++){
+            const num = nums[i]
+            const compliment = target - num
 
-  // loop into the array, and for each number, check which one is the complementary number needed to to reach the target
-  // store the current value, and every time if the
-
-  twoSum(nums, target) {
-    const result = new Map();
-
-    for (let i = 0; i < nums.length; i++) {
-      const complementary = target - nums[i];
-
-      if (result.has(complementary)) {
-        return [i, result.get(complementary)];
-      } else {
-        result.set(nums[i], i);
-      }
+            if(map.has(compliment)){
+                return [map.get(compliment), i]
+            } else{
+                map.set(num, i)
+            }
+        }
     }
-  }
 }
