7b0e9ce03826f191a1dd610996eb5ba24fedd61a
227Faddi <falukhouma27@gmail.com>
2026-03-18 08:26:24 -0400

    chore: update solution

--- neetcode/ProductsofArrayExceptSelf.js
+++ neetcode/ProductsofArrayExceptSelf.js
@@ -3,27 +3,26 @@ class Solution {
      * @param {number[]} nums
      * @return {number[]}
      */
-
     // receive an array of int
-    // return an array of int, which each index will have the sum of the first array minus the value at that index
-    // Input: nums = [1,2,4,6]
-    // output: [48,24,12,8]
+    // return a new array where each value is the product of
+    // all the values expect for the value at that index
+    //Input: nums = [1,2,4,6]
+    //Output: [48,24,12,8]
 
-    // store the product product
-    // loop into the array, and divide for the current index value
-    // and push it into an array
+    // looping into the array, and multuply all values
+    // expect the current index that i'm checking
+    // push the product into an array
 
     productExceptSelf(nums) {
         let result = []
 
+        //Input: nums = [1,2,4,6]
         for(let i = 0; i < nums.length; i++){
             let product = 1
 
             for(let j = 0; j < nums.length; j++){
-                const current = nums[j]
-
                 if(j !== i){
-                    product = product * current 
+                    product = product * nums[j]
                 }
             }
 
