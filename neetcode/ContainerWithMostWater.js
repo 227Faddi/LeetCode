b03e70a70db220765892cebdaee556a9c555fc43
227Faddi <falukhouma27@gmail.com>
2026-03-31 11:35:29 -0400

    chore: update solution

--- neetcode/ContainerWithMostWater.js
+++ neetcode/ContainerWithMostWater.js
@@ -4,19 +4,33 @@ class Solution {
    * @return {number}
    */
 
+  // receive an array of int, not ordered
+  // an array representing the max area
+
+  // [1,7,2,5,4,7,3,6]
+  //  36
+
   maxArea(heights) {
     let max = 0;
 
-    for (let l = 0; l < heights.length - 1; l++) {
-      for (let r = l + 1; r < heights.length; r++) {
-        const length = r - l;
-        const minHeigth = Math.min(heights[l], heights[r]);
+    let l = 0;
+    let r = heights.length - 1;
 
-        const area = length * minHeigth;
+    while (l < r) {
+      const height = Math.min(heights[l], heights[r]);
+      const width = r - l;
+      const area = width * height;
 
-        if (area > max) {
-          max = area;
-        }
+      if (area > max) {
+        max = area;
+      }
+
+      if (heights[l] < heights[r]) {
+        l++;
+      } else if (heights[r] < heights[l]) {
+        r--;
+      } else {
+        l++;
       }
     }
 
