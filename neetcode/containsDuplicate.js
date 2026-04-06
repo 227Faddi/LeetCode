92c1a9a62b9fede379632379a0b4c7b36b1e9f74
227Faddi <falukhouma27@gmail.com>
2026-04-05 22:53:40 -0400

    chore: update solution

--- neetcode/containsDuplicate.js
+++ neetcode/containsDuplicate.js
@@ -3,27 +3,14 @@ class Solution {
    * @param {number[]} nums
    * @return {boolean}
    */
-
-  // input: an array of int
-  // return true if a int is present more than once
-  // [1, 2, 3, 3]
-  // true // 3
-  // [1,2,3]
-  // false, no duplicate
-
-  // checking the array, and count the appearance of each number
-  // then check if the number was already counted, then return true
-  // if we check all the array and all the values are just counted once
-  // return false, because each value is unique
-
   hasDuplicate(nums) {
-    const counts = new Map();
+    const map = new Map();
 
-    for (const num of nums) {
-      if (counts.has(num)) {
+    for (let i = 0; i < nums.length; i++) {
+      if (map.has(nums[i])) {
         return true;
       } else {
-        counts.set(num, 1);
+        map.set(nums[i], i);
       }
     }
 
