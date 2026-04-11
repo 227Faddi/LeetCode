c3f4589016215cd2ef4e47161030839d757912b2
227Faddi <falukhouma27@gmail.com>
2026-04-11 08:49:42 -0400

    chore: update solution

--- neetcode/LongestSubstringWithoutRepeatingCharacters.js
+++ neetcode/LongestSubstringWithoutRepeatingCharacters.js
@@ -4,29 +4,30 @@ class Solution {
    * @return {number}
    */
 
-  // receive a string of letters
-  // return a int representing the longest substring without duplicate charts
+  // receive a string always valid < 0
+  // return an int representing the length of the longest
+  // substring without duplicates
+
   // "zxyzxyz"
   // 3
-  // 2 pointers, one at the beginning
-  // have a counter for the length
 
   lengthOfLongestSubstring(s) {
-    let subString = new Set();
     let max = 0;
+    let subString = new Set();
 
     let l = 0;
     let r = 0;
 
     while (r < s.length) {
-      if (subString.has(s[r])) {
+      while (subString.has(s[r])) {
         subString.delete(s[l]);
         l++;
-      } else {
-        subString.add(s[r]);
-        max = Math.max(max, r - l + 1);
-        r++;
       }
+
+      subString.add(s[r]);
+      max = Math.max(max, r - l + 1);
+
+      r++;
     }
 
     return max;
