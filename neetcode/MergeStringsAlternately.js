5601b96a46f109af8c091a5bf7c94fc512d236b0
227Faddi <falukhouma27@gmail.com>
2026-04-21 09:21:36 -0400

    chore: update solution

--- neetcode/MergeStringsAlternately.js
+++ neetcode/MergeStringsAlternately.js
@@ -5,34 +5,27 @@ class Solution {
    * @return {string}
    */
 
-  // receive 2 string
-  // return a merged string with alternate char
-  //
-  //Input: word1 = "abc", word2 = "xyz"
-  //Output: "axbycz"
-  //Input: word1 = "ab",
-  //       word2 = "abbxxc"
-  //Output: "aabbbxxc"
+  // receive 2 string of lowercase letters
+  // Input: word1 = "abc", word2 = "xyz"
+  // Output: "axbycz"
 
-  // find the smaller string
-  // create 2 pointers that goes from the first to last chart of smaller string
-  // add all char together and then add the rest of the longer string if any
+  // Input: word1 = "ab", word2 = "abbxxc"
+  // Output: "aabbbxxc"
+
   mergeAlternately(word1, word2) {
     let result = "";
-    const max = Math.max(word1.length, word2.length);
-    let l = 0;
+    let i = 0;
+    let j = 0;
 
-    while (l < max) {
-      if (word1[l]) {
-        result += word1[l];
-      }
-
-      if (word2[l]) {
-        result += word2[l];
-      }
-
-      l++;
+    while (i < word1.length && j < word2.length) {
+      result += word1[i];
+      result += word2[j];
+      i++;
+      j++;
     }
+
+    result += word1.slice(i);
+    result += word2.slice(j);
 
     return result;
   }
