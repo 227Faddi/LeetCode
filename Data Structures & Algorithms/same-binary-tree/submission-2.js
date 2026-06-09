6820d1be72b111e412288252e29d06160e7873d1
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-06-09 09:02:41 -0400

    Add: same-binary-tree - submission-2

---
+++ Data Structures & Algorithms/same-binary-tree/submission-2.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// receive 2 binary trees
+// a ds that is formed by nodes with at max 2 children
+// return a boolean indicating whether they are the same or not
+// to know if they are 
+
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+        if(!p && !q) return true
+
+        if(!p || !q) return false
+
+        if(p.val === q.val){
+            let leftSame = this.isSameTree(p.left, q.left)
+            let rightSame = this.isSameTree(p.right, q.right)
+
+            return leftSame && rightSame
+        }
+
+        return false
+    }
+}
