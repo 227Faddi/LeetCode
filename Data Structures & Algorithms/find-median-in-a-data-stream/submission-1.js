629dbb74cee73d1227a4a467de1623d2fdd7e8cc
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-07-17 08:58:40 -0400

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.js
@@ -0,0 +1,29 @@
+class MedianFinder {
+    constructor() {
+        this.arr = []
+    }
+
+    /**
+     *
+     * @param {number} num
+     * @return {void}
+     */
+    addNum(num) {
+        this.arr.push(num)
+        this.arr.sort((a, b) => a - b)
+    }
+
+    /**
+     * @return {number}
+     */
+    findMedian() {
+        if(this.arr.length % 2 === 0){
+            const i = this.arr[Math.floor(this.arr.length / 2)]
+            const x = this.arr[Math.floor(this.arr.length / 2) - 1]
+            
+            return (i + x) / 2
+        } else {
+            return this.arr[Math.floor(this.arr.length / 2)]
+        }
+    }
+}
