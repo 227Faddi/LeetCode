5bb3cca4642812a98938b346000b27367ce1a895
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-07-30 23:53:44 -0400

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.js
@@ -0,0 +1,57 @@
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+  }
+}
+
+class PrefixTree {
+    constructor() {
+        this.root = new TrieNode()
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {
+        let node = this.root
+        for(const c of word){
+            if(!node.children[c]){
+                node.children[c] = new TrieNode()
+            }
+            node = node.children[c]
+        }
+        node.isEndOfWord = true
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        let node = this.root
+        for(const c of word){
+            if(!node.children[c]){
+                return false
+            }
+            node = node.children[c]
+        }
+        return node.isEndOfWord
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {
+        let node = this.root
+        for(const c of prefix){
+            if(!node.children[c]){
+                return false
+            }
+            node = node.children[c]
+        }
+        return true
+    }
+}
