9355f36e7bbcad531c77715be6c42548c20082e4
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-08-09 10:21:51 -0400

    Add: duplicate-integer - submission-9

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        let s = new Set()
+
+        for(const n of nums){
+            if(s.has(n)) return true
+            s.add(n)
+        }
+
+        return false
+    }
+}
