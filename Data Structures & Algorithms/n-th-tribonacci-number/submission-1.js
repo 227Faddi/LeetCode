fe5be0917128dbad9c894ae55f25deed073db7c6
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-08-24 10:10:51 -0400

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    tribonacci(n) {
+        let tab = new Array(n).fill(0)
+
+        tab[0] = 0
+        tab[1] = 1
+        tab[2] = 1
+
+        for(let i = 3; i < n + 1; i++){
+            tab[i] = tab[i - 3] + tab[i - 2] + tab[i - 1]
+        }
+
+        return tab[n]
+    }
+}
