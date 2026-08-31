e8269e7f0362e29be0ab81b94e02224acb122fc2
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-08-30 22:12:45 -0400

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        let memo = new Array(n).fill(-1) 
+
+        function dp(i){
+            if(i === n) return 1 
+            if(i > n) return 0
+            if(memo[i] !== -1) return memo[i]
+
+
+            memo[i] = dp(i + 1) + dp(i + 2)
+            return memo[i]
+        }
+
+        return dp(0)
+    }
+}
