644c9c6e640d2556d8be62d602b8ed6483afb123
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-09-01 09:02:47 -0400

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+
+    rob(nums) {
+        if (nums.length === 1) return nums[0];
+        return Math.max(this.dp(nums.slice(0, nums.length - 1)), this.dp(nums.slice(1)));
+    }
+
+    dp(nums) {
+        let one = 0
+        let two = 0
+
+        for(const num of nums){
+            let newNum = Math.max(one + num, two)
+            one = two
+            two = newNum
+        }
+
+        return two
+    }
+}
