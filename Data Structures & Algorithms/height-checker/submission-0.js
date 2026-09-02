a5fa92ea79f1d3ed7f7ffd113261fc37d84b0164
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-09-02 08:42:48 -0400

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    heightChecker(heights) {
+        const sorted = [...heights].sort((a, b) => a - b)
+        let res = 0
+
+        console.log(heights)
+        console.log(sorted)
+
+
+        for(let i = 0; i < heights.length; i++){
+            console.log(sorted[i], heights[i])
+            if(sorted[i] !== heights[i]){
+                res++
+            }
+        }
+
+        return res
+    }
+}
