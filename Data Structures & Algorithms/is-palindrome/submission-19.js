3aa03198a9667f15c98e709d37dd7179ce11ea24
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-09-02 09:08:44 -0400

    Add: is-palindrome - submission-19

---
+++ Data Structures & Algorithms/is-palindrome/submission-19.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const newS = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+        let l = 0
+        let r = newS.length - 1 
+        
+        while(l < r){
+            if(newS[l] !== newS[r]) return false
+            l++
+            r--
+        }
+
+        return true
+    }
+}
