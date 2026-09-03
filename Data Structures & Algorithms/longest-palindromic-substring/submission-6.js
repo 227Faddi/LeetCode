a05448641d599d7f7540e38d5f04b79b45719c04
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-09-03 08:56:06 -0400

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+        let res = ""
+
+        function isPalindrome(l, r){
+            while(l >= 0 && r < s.length && s[l] === s[r]){
+                if((r - l + 1) > res.length){
+                    res = s.slice(l, r + 1)
+                }
+                l--
+                r++
+            }
+        }
+
+        for(let i = 0; i < s.length; i++){
+            let l = i
+            let r = i
+            isPalindrome(l, r)
+            l = i
+            r = i + 1
+            isPalindrome(l, r)
+        }
+
+        
+        return res
+    }
+}
