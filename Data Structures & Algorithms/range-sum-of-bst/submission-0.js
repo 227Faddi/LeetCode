f63850929bf410366837ebfc06ff71516dada48f
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-09-11 14:50:09 -0400

    Add: range-sum-of-bst - submission-0

---
+++ Data Structures & Algorithms/range-sum-of-bst/submission-0.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} low
+     * @param {number} high
+     * @return {number}
+     */
+    rangeSumBST(root, low, high) {
+        let res = 0
+
+        function dfs(node){
+            if(!node) return null
+
+            if(node.val <= high && node.val >= low){
+                res += node.val
+            }
+
+            dfs(node.left)
+            dfs(node.right)
+        }
+
+        dfs(root)
+
+        return res
+    }
+}
