8cee878570e7f0550f1368d460a85cdb9f01199d
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-09-14 08:46:58 -0400

    Add: depth-of-binary-tree - submission-7

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-7.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root) {
+        let q = new Queue()
+        let visited = new Set()
+        let res = 0
+
+        if(!root) return res
+
+        q.enqueue(root)
+
+        while(!q.isEmpty()){
+            let size = q.size()
+
+            for(let i = 0; i < size; i++){
+                const node = q.dequeue()
+                console.log(node.val)
+                if(node.left){
+                    q.enqueue(node.left)
+                }
+                if(node.right){
+                    q.enqueue(node.right)
+                }
+            }
+
+            res++
+        }
+
+        return res
+    }
+}
