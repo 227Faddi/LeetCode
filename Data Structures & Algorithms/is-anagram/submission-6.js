5d17aac7389bb2de4b25e4fc902fc949476b96f9
Falilou Khouma <126704343+227Faddi@users.noreply.github.com>
2026-09-18 15:10:23 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        let sMap = new Map()
+        let tMap = new Map()
+
+        for (let i = 0; i < s.length; i++) {
+            sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
+            tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
+        }
+
+        for (let [key, count] of sMap) {
+            if (tMap.get(key) !== count) {
+                return false;
+            }
+        }   
+
+        return true
+    }
+}
